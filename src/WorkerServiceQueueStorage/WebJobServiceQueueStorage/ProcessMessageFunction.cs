12aeb267433afbf65bcb0b156d31fcc37b2834c8
bufaonanet <douglas.bufaonanet@outlook.com>
2023-02-14 11:10:18 -0300

    add webjob project

---
+++ src/WorkerServiceQueueStorage/WebJobServiceQueueStorage/ProcessMessageFunction.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobServiceQueueStorage;
+
+public class ProcessMessageFunction
+{
+    private readonly QueueClient _queueClient;
+
+    public ProcessMessageFunction(IConfiguration configuration)
+    {
+        _queueClient = new QueueClient(configuration["AzureWebJobsStorage"], "processada");
+    }
+
+    public async Task ProcessQueueeMessage([QueueTrigger("minhafila")] string message, ILogger logger)
+    {
+        await _queueClient.SendMessageAsync($"Processed {message} {DateTime.Now}");
+        logger.LogInformation(message);
+    }
+}
---
+++ src/WorkerServiceQueueStorage/WebJobServiceQueueStorage/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = new HostBuilder();
+
+//builder.UseEnvironment(EnvironmentName.Development);
+
+builder.ConfigureLogging((context, b) =>
+{
+    b.AddConsole();
+});
+
+builder.ConfigureWebJobs(b =>
+{
+    b.AddAzureStorageCoreServices();
+    b.AddAzureStorageQueues();
+});
+
+
+var host = builder.Build();
+using (host)
+{
+    await host.RunAsync();
+}
